3ca69852b045cd5b06e99615929d2d279f128e24
linyiyuan <linyiyuann@gmail.com>
2020-11-26 19:19:19 +0800

    完善登陆逻辑

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,4 @@
-import { login, logout, getInfo } from '@/api/auth/login'
+import { login, logout, initialization } from '@/api/auth/login'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import store from '@/store'
 
@@ -69,7 +69,7 @@ const user = {
     // 获取用户信息
     GetInfo({ commit, state }) {
       return new Promise((resolve, reject) => {
-        getInfo().then(response => {
+        initialization().then(response => {
           const data = response.data
           if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
             commit('SET_ROLES', data.role)
