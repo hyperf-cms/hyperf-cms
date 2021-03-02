58684efa677ee627cf491ca396bff0ad4c919c17
linyiyuan <linyiyuann@gmail.com>
2021-03-02 14:23:20 +0800

    提交配置文件

---
+++ config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://192.168.6.84:81/"',
+  SITENAME: 'Vue-admin-template',
+})
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://192.168.6.84:81/"',
+}
