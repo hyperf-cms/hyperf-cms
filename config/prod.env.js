4c194100c5ec24207d71f43c1e3b403d2ec93eae
linyiyuan <linyiyuann@gmail.com>
2021-05-21 15:36:06 +0800

    更新包

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.6.84:81/"',
-  WS_API: '"192.168.6.84:9502"',
+  BASE_API: '"http://hyperf.linyiyuan.top"',
+  WS_API: '"ws://ws.linyiyuan.top"',
 }
